751e420be5ec5162e7e2e898d3a0faa8b54f314a
FabianPonczko <fabianponczko@live.com.ar>
2024-06-10 22:25:29 -0300

    lista para deploy

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-
-}
-
-module.exports = nextConfig
+    reactStrictMode: true,
+    compiler: {
+      styledComponents: true,
+    },
+  }
+  
+  module.exports = nextConfig
