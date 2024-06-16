e044171f8509412afc05e7f97baa3a256191d9e9
FabianPonczko <fabianponczko@live.com.ar>
2024-06-16 04:09:54 -0300

    cambio nombre url 1

--- components/Header.js
+++ components/Header.js
@@ -69,13 +69,13 @@ export default function Header() {
     <StyledHeader>
       <Center>
         <Wrapper>
-          <Logo href={'/'}>Ecommerce</Logo>
+          <Logo href={'/'}>Tienda golden !</Logo>
             <StyledNav mobileNavActive={mobileNavActive}>
             <NavLink href={'/'}>Home</NavLink>
-            <NavLink href={'/products'}>All products</NavLink>
-            <NavLink href={'/categories'}>Categories</NavLink>
-            <NavLink href={'/account'}>Account</NavLink>
-            <NavLink href={'/cart'}>Cart ({cartProducts.length})</NavLink>
+            <NavLink href={'/products'}>Productos</NavLink>
+            <NavLink href={'/categories'}>Categorias</NavLink>
+            <NavLink href={'/account'}>Datos de Usuario</NavLink>
+            <NavLink href={'/cart'}>Carrito ({cartProducts.length})</NavLink>
           </StyledNav>
           <NavButton onClick={() => setMobileNavActive(prev => !prev)}>
             <BarsIcon />
