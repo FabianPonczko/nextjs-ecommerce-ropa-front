5c7450002cb508750c1177b63a574df6166a4491
FabianPonczko <fabianponczko@live.com.ar>
2024-06-16 01:23:13 -0300

    fata 102

--- pages/api/notification.js
+++ pages/api/notification.js
@@ -74,7 +74,7 @@ if (sha === hash) {
     if (dataID.type==="payment"){
         const payment =  new Payment(client)
         payment.get({id:dataID["data.id"]}).then((data=>{
-            const id = data.external_reference.orderId
+            const id = data.external_reference
             console.log("id : ",id)
              getdata(id)
         })
