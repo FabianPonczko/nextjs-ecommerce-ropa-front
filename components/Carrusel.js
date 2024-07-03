e7c61e5dea29a48ccbbb6f764fd884c1ca9b6700
FabianPonczko <fabianponczko@live.com.ar>
2024-07-03 00:01:53 -0300

    carusel 5

--- components/Carrusel.js
+++ components/Carrusel.js
@@ -1,8 +1,26 @@
+import styled from "styled-components";
 import React from 'react';
 import Slider from 'react-slick';
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 
+const Estilo = styled.div`
+  width:100vw;
+  height:60vh;
+  @media screen and (min-width: 768px) {
+  width:100vw;
+  height:80vh;
+  }
+`;
+const EstiloiN = styled.div`
+  max-width:100vw;
+  max-height:60vw;
+  
+  @media screen and (min-width: 768px) {
+  width:100vw;
+  height:80vh;
+  }
+`;
 const Carousel = () => {
   const settings = {
     dots: true,
@@ -16,19 +34,28 @@ const Carousel = () => {
   };
 
   return (
-    <div style={{width:"100vw",height:"80vh"}}>
+    
+          
+
+
       <Slider {...settings}>
-        <div>
-          <img src="img/image2.jpg" alt="Slide 1" style={{width:"100vw",height:"80vh"}} />
-        </div>
-        <div>
-          <img src="img/image1.jpg" alt="Slide 2" style={{width:"100vw",height:"80vh"}} />
-        </div>
+
+
+        
+          <img src="img/image2.jpg" alt="Slide 1"  />
+        
+    
+        
+          <img src="img/image1.jpg" alt="Slide 2" />
+        
+        
+        
         {/* <div> */}
           {/* <img src="img/image2.jpg" alt="Slide 3" style={{width:"100vw",height:"80vh"}} /> */}
         {/* </div> */}
       </Slider>
-    </div>
+    
+    
   );
 };
 
